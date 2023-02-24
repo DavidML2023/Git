1d4357587fdfeded8217414eb20e60294e799506
David <angeldavidmatosleyva@gmail.com>
2023-02-24 16:59:03 -0500

    parrafo_tamaño

--- index.php
+++ index.php
@@ -4,9 +4,12 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
 </head>
 <body>
 	<h1>Hola Curso</h1>
 	<p>Ejemplo Git</p>
+	<h3>Proyectos</h3>
+	
 </body>
 </html>
