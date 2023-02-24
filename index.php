133d2453179e8966dec919657698218f38700b92
David <angeldavidmatosleyva@gmail.com>
2023-02-24 12:20:51 -0500

    Titulo_Parrafo

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+	<h1>Hola Curso</h1>
+	<p>Ejemplo Git</p>
+</body>
+</html>
